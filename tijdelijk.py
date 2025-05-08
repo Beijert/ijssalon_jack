87b5b3d4c3d31f13bb57bc6dfb9cb33903e87db5
Jack <jack_beijer@hotmail.com>
2025-05-08 09:09:38 +0200

    fucties toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,34 +1,27 @@
-#opdracht 2
-prijzen = {
-    "aardbei" : 3,
-    "Vanille" : 4,
-    "Chocolade" : 5
-}
+from helper import decoreer
 
-#opdracht 3
-aanbieding = prijzen["aardbei"] *0.8
+def print_aanbiedingen():
+    prijzen = {
+        "aardbei" : 3,
+        "Vanille" : 4,
+        "Chocolade" : 5
+    }
 
-#opdracht 4
-#In de opdracht staat dat de aanbieding "vanille-ijs" zou moeten zijn, echter klopt dit dan niet met de bovenstaande code
-reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter – slechts € {aanbieding}"
-#print(reclame_tekst)
+    aanbieding = prijzen["aardbei"] *0.8
 
-#opdracht 5
-reclame_tekst2 = reclame_tekst[:62]
-#print(reclame_tekst2)
+    reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter – slechts € {aanbieding}"
 
-#opdracht 6
-reclame_tekst3 = (reclame_tekst2.upper())
-#print(reclame_tekst3)
+    reclame_tekst2 = reclame_tekst[:62]
 
-#opdracht 7
-reclame_tekst4 = (reclame_tekst3.split(" "))
-#print(reclame_tekst4)
+    reclame_tekst3 = (reclame_tekst2.upper())
 
-#opdracht 8-10
-for el in reclame_tekst4:
-    if len(el) > 4:
-        print(el.upper())
-    else:
-        print(el.lower())
-    
+    reclame_tekst4 = (reclame_tekst3.split(" "))
+
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("aanbieding")
+print_aanbiedingen()
