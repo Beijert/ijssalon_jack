da519b17c341daef43e1f187fd7238dcf775d896
Jack <jack_beijer@hotmail.com>
2025-05-12 18:15:30 +0200

    Les 10.7

--- helper.py
+++ helper.py
@@ -1,7 +1,22 @@
 def decoreer(tekst = ""):
+    tekst = "header"
     lengte = len(tekst) + 4
     print()
     print(lengte * "*")
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag,personen):
+    try:
+        fooi = bedrag/personen
+    except:
+        fooi = "??"
+    pp = f"Het bedrag per persoon is {fooi} euro"
+    return pp
+
+def onderstreep(tekst= ""):
+    uit = []
+    uit.append(tekst)
+    uit.insert(1, len(tekst) * "=")
+    return
